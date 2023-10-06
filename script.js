66fad05ca3918659584058b83934e2993c0658cc
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-10-06 23:09:48 +0200

    added front

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const URL = `https://restcountries.com/v3.1/name/${country}`;
+const rightArrow = document.querySelector(".fa-arrow-right");
+
+rightArrow.addEventListener("click", function (country) {
+  fetch("https://restcountries.com/v3.1/name/${country}")
+    .then((resp) => resp.json())
+    .then((data) => console.log(data));
+});
