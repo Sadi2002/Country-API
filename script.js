efab2cbf0baea8fc5943c6f0e65b94de9ca00452
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-10-06 23:41:43 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// FETCHING ELEMENTS
 const rightArrow = document.querySelector(".fa-arrow-right");
 
-rightArrow.addEventListener("click", function (country) {
-  fetch("https://restcountries.com/v3.1/name/${country}")
+// FUNCTION WHICH GET NAME FROM GetCountry AND CREATE CARD BASED ON IT
+const createCard = function (country) {
+  console.log(country);
+};
+
+// FUNCTION WHICH CREATE API AND SEND IT TO ceateCard FUNCTION
+const getCountry = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then((resp) => resp.json())
-    .then((data) => console.log(data));
-});
+    .then((data) => createCard(data[0]));
+};
+
+// CALLING
+getCountry("usa");
